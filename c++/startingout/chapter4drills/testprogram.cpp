092061deceee1d2290f9efef08796ac431126890
Daniel Burgess <danielb2811@gmail.com>
2015-06-14 17:07:26 +0100

    Practice updates

---
+++ c++/startingout/chapter4drills/testprogram.cpp
@@ -0,0 +1,13 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    if (true)
+    {
+        cout << "This is the first if statement.\n";
+    }
+    if (true)
+    {
+        cout << "This is the second if statement.\n";
+    }
+}
--- c++/startingout/chapter4drills/twointwhileloop.cpp
+++ c++/startingout/chapter4drills/twointwhileloop.cpp
@@ -1,16 +1,29 @@
 #include "../std_lib_facilities.h"
 
-int main() {
-    int number1 = 0;
-    int number2 = 0;
+int main()
+{
+    double number1 = 0;
+    double number2 = 0;
     cout << "Enter a digit to print, or | to exit: \n";
-    while (cin>>number1>>number2) { 
-        if (number1==number2)
+    while (cin>>number1>>number2)
+    { 
+        double numberdifference;
+        double absnumberdifference;
+        numberdifference = number1 - number2;
+        absnumberdifference = fabs(numberdifference);
+
+        cout << "The absnumberdifference variable is equal to: " << absnumberdifference << '\n';
+
+        if (number1==number2) 
             cout << "The numbers are equal.\n";
         else if (number1>number2)
             cout << number1 << " is greater than " << number2 << ".\n";
         else if (number2>number1)
             cout << number2 << " is greater than " << number1 << ".\n";
+
+        if (0.01==absnumberdifference || 0.01>absnumberdifference)
+            cout << "The numbers are almost equal.\n";
+
         cout << "Enter a digit to print, or | to exit: \n";
     }
 }
