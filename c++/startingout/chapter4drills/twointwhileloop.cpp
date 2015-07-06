488b19cd8d9d4cdec84ba67c4687fe1144ec96b2
Daniel Burgess <danielb2811@gmail.com>
2015-07-06 22:51:44 +0100

    drill update

--- c++/startingout/chapter4drills/twointwhileloop.cpp
+++ c++/startingout/chapter4drills/twointwhileloop.cpp
@@ -1,29 +1,26 @@
-#include "../std_lib_facilities.h"
+#include<iostream>
+#include<cmath>
 
 int main()
 {
     double number1 = 0;
     double number2 = 0;
-    cout << "Enter a digit to print, or | to exit: \n";
-    while (cin>>number1>>number2)
-    { 
-        double numberdifference;
-        double absnumberdifference;
-        numberdifference = number1 - number2;
-        absnumberdifference = fabs(numberdifference);
+    std::cout << "Enter a digit to print, or | to exit: \n";
+    while (std::cin>>number1>>number2)
+    {
 
-        cout << "The absnumberdifference variable is equal to: " << absnumberdifference << '\n';
+        std::cout << "The absolute difference between the two numbers is: " << fabs(number1-number2) << '\n';
 
         if (number1==number2) 
-            cout << "The numbers are equal.\n";
+            std::cout << "The numbers are equal.\n";
         else if (number1>number2)
-            cout << number1 << " is greater than " << number2 << ".\n";
+            std::cout << number1 << " is greater than " << number2 << ".\n";
         else if (number2>number1)
-            cout << number2 << " is greater than " << number1 << ".\n";
+            std::cout << number2 << " is greater than " << number1 << ".\n";
 
-        if (absnumberdifference<=0.01)
-            cout << "The numbers are almost equal.\n";
+        if (fabs(number1-number2)<=.01)
+            std::cout << "The numbers are almost equal.\n";
 
-        cout << "Enter a digit to print, or | to exit: \n";
+        std::cout << "Enter a digit to print, or | to exit: \n";
     }
 }
